f335395f6c26d4a4870ce05d34987da0a9fccf1a
appkit-framework <admin@appkit.biz>
2026-02-11 13:46:25 +0100

    Logging with context

--- src/JobQueue.php
+++ src/JobQueue.php
@@ -13,7 +13,7 @@ use function AppKit\Async\async;
 use Throwable;
 
 class JobQueue implements StartStopInterface, HealthIndicatorInterface {
-    const AMQP_PREFIX = 'appkit_jobqueue';
+    const AMQP_PREFIX = 'ak_jq';
 
     private $appId;
     private $amqp;
@@ -31,18 +31,27 @@ class JobQueue implements StartStopInterface, HealthIndicatorInterface {
     }
 
     public function start() {
+        $exchange = self::AMQP_PREFIX;
+
         try {
             $this -> amqp -> declareExchange(
-                self::AMQP_PREFIX,
+                $exchange,
                 'direct',
                 false, // passive
                 true, // durable
                 false // autoDelete
             );
-            $this -> log -> debug('Declared job queue exchange');
+            $this -> log -> debug(
+                'Declared exchange',
+                [ 'exchange' => $exchange ]
+            );
         } catch(Throwable $e) {
-            $error = 'Failed to declare job queue exchange';
-            $this -> log -> error($error, $e);
+            $error = 'Failed to declare exchange';
+            $this -> log -> error(
+                $error,
+                [ 'exchange' => $exchange ],
+                $e
+            );
             throw new JobQueueException(
                 $error,
                 previous: $e
@@ -62,12 +71,22 @@ class JobQueue implements StartStopInterface, HealthIndicatorInterface {
         $this -> isStarted = false;
 
         foreach($this -> workerRestoreData as $job => $_) {
-            $this -> log -> warning("Worker $job is still active at shutdown");
+            $this -> log -> warning(
+                'Worker is still active at shutdown',
+                [ 'job' => $job ]
+            );
             try {
                 $this -> cancelWorker($job);
-                $this -> log -> info("Canceled worker $job");
+                $this -> log -> info(
+                    'Canceled worker',
+                    [ 'job' => $job ]
+                );
             } catch(Throwable $e) {
-                $this -> log -> error("Failed to cancel worker $job", $e);
+                $this -> log -> error(
+                    'Failed to cancel worker',
+                    [ 'job' => $job ],
+                    $e
+                );
             }
         }
     }
@@ -79,7 +98,7 @@ class JobQueue implements StartStopInterface, HealthIndicatorInterface {
         ]);
     }
 
-    public function enqueue($appId, $job, $body = [], $ttl = 0) {
+    public function dispatch($appId, $job, $body = [], $ttl = 0) {
         $headers = [
             'delivery_mode' => $ttl == 0 ? 2 : 1, // 1=transient, 2=persistent
             'expiration' => (string)($ttl * 1000)
@@ -94,18 +113,30 @@ class JobQueue implements StartStopInterface, HealthIndicatorInterface {
             );
         }
 
+        $exchange = self::AMQP_PREFIX;
+        $routingKey = $appId . '_' . $job;
         try {
             return $this -> amqp -> publish(
                 $bodyJson,
                 $headers,
-                self::AMQP_PREFIX,
-                "${appId}_$job",
+                $exchange,
+                $routingKey,
                 true, // mandatory
                 confirm: true
             );
         } catch(Throwable $e) {
             $error = 'Failed to publish AMQP message';
-            $this -> log -> error($error, $e);
+            $this -> log -> error(
+                $error,
+                [
+                    'appId' => $appId,
+                    'job' => $job,
+                    'headers' => $headers,
+                    'exchange' => $exchange,
+                    'routingKey' => $routingKey
+                ],
+                $e
+            );
             throw new JobQueueException(
                 $error,
                 previous: $e
@@ -120,34 +151,43 @@ class JobQueue implements StartStopInterface, HealthIndicatorInterface {
         $prefetchCount = null
     ) {
         if(isset($this -> workerRestoreData[$job]))
-            throw new JobQueueException("Worker $job already exists");
+            throw new JobQueueException("Already working on job $job");
 
         $workerRestoreData = [
-            $job,
             $handler,
             $concurrency,
             $prefetchCount
         ];
 
-        $this -> workInternal(...$workerRestoreData);
+        $this -> workInternal($job, ...$workerRestoreData);
         $this -> workerRestoreData[$job] = $workerRestoreData;
 
-        $this -> log -> debug("New worker: $job");
+        $this -> log -> debug(
+            'Created worker',
+            [
+                'job' => $job,
+                'concurrency' => $concurrency,
+                'prefetchCount' => $prefetchCount
+            ]
+        );
 
         return $this;
     }
 
     public function cancelWorker($job) {
         if(!isset($this -> workerRestoreData[$job]))
-            throw new JobQueueException("Worker $job does not exist");
+            throw new JobQueueException("No active worker for job $job");
 
         if(isset($this -> workerCtag[$job])) {
-            $this -> cancelWorkerInternal($this -> workerCtag[$job]);
+            $this -> cancelWorkerInternal($this -> workerCtag[$job], $job);
             unset($this -> workerCtag[$job]);
         }
 
         unset($this -> workerRestoreData[$job]);
-        $this -> log -> debug("Cleaned up worker $job");
+        $this -> log -> debug(
+            'Cleaned up worker',
+            [ 'job' => $job ]
+        );
 
         return $this;
     }
@@ -158,8 +198,8 @@ class JobQueue implements StartStopInterface, HealthIndicatorInterface {
         $concurrency,
         $prefetchCount
     ) {
-        $routingKey = $this -> appId . "_$job";
-        $queue = self::AMQP_PREFIX . "_$routingKey";
+        $routingKey = $this -> appId . '_' . $job;
+        $queue = self::AMQP_PREFIX . '_' . $routingKey;
         if(strlen($queue) > 255)
             $queue = self::AMQP_PREFIX . '_' . hash('sha256', $routingKey);
 
@@ -171,29 +211,54 @@ class JobQueue implements StartStopInterface, HealthIndicatorInterface {
                 false, // exclusive
                 false // autoDelete
             );
-            $this -> log -> debug("Declared queue $queue");
+            $this -> log -> debug(
+                'Declared worker queue',
+                [ 'queue' => $queue, 'job' => $job ]
+            );
         } catch(Throwable $e) {
-            $error = 'Failed to declare queue';
-            $this -> log -> error("$error $queue", $e);
+            $error = 'Failed to declare worker queue';
+            $this -> log -> error(
+                $error,
+                [ 'queue' => $queue, 'job' => $job ],
+                $e
+            );
             throw new JobQueueException($error, previous: $e);
         }
 
+        $exchange = self::AMQP_PREFIX;
+
         try {
-            $logMsg = "$queue to exchange " . self::AMQP_PREFIX . " by routing key $routingKey";
             $this -> amqp -> bindQueue(
                 $queue,
-                self::AMQP_PREFIX,
+                $exchange,
                 $routingKey
             );
-            $this -> log -> debug("Bound queue $logMsg");
+            $this -> log -> debug(
+                'Bound worker queue',
+                [
+                    'queue' => $queue,
+                    'exchange' => $exchange,
+                    'routingKey' => $routingKey,
+                    'job' => $job
+                ]
+            );
         } catch(Throwable $e) {
-            $error = 'Failed to bind queue';
-            $this -> log -> error("$error $logMsg", $e);
+            $error = 'Failed to bind worker queue';
+            $this -> log -> error(
+                $error,
+                [
+                    'queue' => $queue,
+                    'exchange' => $exchange,
+                    'routingKey' => $routingKey,
+                    'job' => $job
+                ],
+                $e
+            );
             throw new JobQueueException($error, previous: $e);
         }
 
         try {
-            $ctag = $this -> amqp -> consume(
+            $consumerTag = $this -> amqp -> consume(
                 $queue,
                 function($body, $headers) use($handler, $job) {
                     return $this -> handleMessage($body, $headers, $handler, $job);
@@ -201,65 +266,98 @@ class JobQueue implements StartStopInterface, HealthIndicatorInterface {
                 concurrency: $concurrency,
                 prefetchCount: $prefetchCount
             );
-            $this -> log -> debug("Consumed queue $queue, consumer tag: $ctag");
+            $this -> log -> debug(
+                'Started consumer',
+                [
+                    'consumerTag' => $consumerTag,
+                    'queue' => $queue,
+                    'concurrency' => $concurrency,
+                    'prefetchCount' => $prefetchCount,
+                    'job' => $job
+                ]
+            );
         } catch(Throwable $e) {
-            $error = 'Failed to consume queue';
-            $this -> log -> error("$error $queue");
+            $error = 'Failed to start consumer';
+            $this -> log -> error(
+                $error,
+                [
+                    'queue' => $queue,
+                    'concurrency' => $concurrency,
+                    'prefetchCount' => $prefetchCount,
+                    'job' => $job
+                ],
+                $e
+            );
             throw new JobQueueException($error, previous: $e);
         }
 
-        $this -> workerCtag[$job] = $ctag;
+        $this -> workerCtag[$job] = $consumerTag;
     }
 
-    private function cancelWorkerInternal($ctag) {
+    private function cancelWorkerInternal($consumerTag, $job) {
         try {
-            $this -> amqp -> cancelConsumer($ctag);
-            $this -> log -> debug("Canceled consumer $ctag");
+            $this -> amqp -> cancelConsumer($consumerTag);
+            $this -> log -> debug(
+                'Canceled consumer',
+                [
+                    'consumerTag' => $consumerTag,
+                    'job' => $job
+                ]
+            );
         } catch(Throwable $e) {
-            $error = "Failed to cancel consumer";
-            $this -> log -> error("$error $ctag", $e);
+            $error = 'Failed to cancel consumer';
+            $this -> log -> error(
+                $error,
+                [
+                    'consumerTag' => $consumerTag,
+                    'job' => $job
+                ],
+                $e
+            );
             throw new JobQueueException($error, previous: $e);
         }
     }
 
     private function onAmqpReconnect() {
-        $this -> log -> warning("Detected AMQP client reconnect, restoring all workers...");
+        $this -> log -> warning("Detected AMQP client reconnect, restoring all workers");
 
         foreach($this -> workerRestoreData as $job => $restoreData) {
             try {
-                $this -> workInternal(...$restoreData);
-                $this -> log -> info("Restored worker $job");
+                $this -> workInternal($job, ...$restoreData);
+                $this -> log -> info(
+                    'Restored worker',
+                    [ 'job' => $job ]
+                );
             } catch(Throwable $e) {
-                $this -> log -> error("Failed to restore worker $job", $e);
+                $this -> log -> error(
+                    'Failed to restore worker',
+                    [ 'job' => $job ],
+                    $e
+                );
             }
         }
     }
 
     private function handleMessage($bodyJson, $headers, $handler, $job) {
-        $messageIdStr = $headers['message-id'] ?? 'missing message-id';
+        $this -> log -> setContext('jqJob', $job);
 
         try {
             $body = Json::decode($bodyJson);
         } catch(Throwable $e) {
-            $this -> log -> warning(
-                "Rejecting corrupted job ${job}[$messageIdStr]",
-                $e
-            );
-            throw new AmqpNackReject(
-                "Failed to decode message",
-                previous: $e
-            );
+            $error = 'Failed to decode message';
+            $this -> log -> warning($error, $e);
+            throw new AmqpNackReject($error, previous: $e);
         }
 
         try {
             $handler($body);
         } catch(Throwable $e) {
             $this -> log -> error(
-                "Requeuing failed job ${job}[$messageIdStr]",
+                'Uncaught worker exception, requeuing task',
                 $e
             );
             throw new AmqpNackRequeue(
-                "Job failed",
+                'Worker exception',
                 previous: $e
             );
         }
